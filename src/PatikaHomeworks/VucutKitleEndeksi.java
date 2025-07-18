13f3e76a4403bb3874cf7424a71723bf86ec44f4
ASENA ARIKAN <asena_ozkoc@hotmail.com>
2025-07-18 22:34:20 +0300

    Vucut Kitle İndeksi ve Manav Kasa Program

---
+++ src/PatikaHomeworks/VucutKitleEndeksi.java
@@ -0,0 +1,26 @@
+package PatikaHomeworks;
+
+import java.util.Scanner;
+
+public class VucutKitleEndeksi {
+
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+//boy girişi yapılması isteğimi yazdırıyorum
+        System.out.println("Lütfen boyunuzu (metre cinsinde) giriniz: ");
+        // giriş yapılabilmesi için boy değişkeni tanımladım reel sayı
+        double boy = scanner.nextDouble();
+// kilo girişi yapılmasını istediğimi yazdırıyorum
+        System.out.println("Lütfen kilonuzu giriniz: ");
+        // giriş yapılabilmesi için kilo değişkeni tanımladım reel sayı
+        double kilo = scanner.nextDouble();
+        // vücut kitle indeksini bulduran işlemi vki adında değişkene reel sayı olarak atadım
+
+        double vki = kilo / (boy * boy);
+        // ekrana yazdırıyorum
+        System.out.println("Vücut Kitle İndeksiniz: " + vki );
+
+        scanner.close();
+    }
+}
