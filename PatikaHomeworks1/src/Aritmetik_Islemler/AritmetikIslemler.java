12ae8ec48b7c13bf064796f432aa9844878298d6
ASENA ARIKAN <asena_ozkoc@hotmail.com>
2025-07-24 15:59:45 +0300

    Update AritmetikIslemler.java

--- PatikaHomeworks1/src/Aritmetik_Islemler/AritmetikIslemler.java
+++ PatikaHomeworks1/src/Aritmetik_Islemler/AritmetikIslemler.java
@@ -4,7 +4,8 @@ public class AritmetikIslemler {
 
         public static void main (String[] args) {
             Scanner scanner = new Scanner(System.in);
-
+                
+//kullanicidan 3 farkli sayi alinir ve tanımlanir
             System.out.print("Bir sayı giriniz: ");
             int a = scanner.nextInt();
 
@@ -14,6 +15,8 @@ public class AritmetikIslemler {
             System.out.print("Üçüncü bir sayı giriniz: ");
             int c = scanner.nextInt();
 
+                
+// islem tanımlanarak yazdirilir.
             double sonuc = a + b * c - b;
             System.out.print("Result: " + sonuc);
 
