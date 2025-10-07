866ef093569381980c526c59e3dfed55874714f8
ASENA ARIKAN <asena_ozkoc@hotmail.com>
2025-10-07 21:02:02 +0300

    yorum satırları eklendi

--- PatikaHomeworks1/src/EnumsHomework/Main.java
+++ PatikaHomeworks1/src/EnumsHomework/Main.java
@@ -7,8 +7,10 @@ public class Main {
         Scanner input = new Scanner(System.in);
 
         System.out.println("Haftanın bir gününü giriniz (örnek: MONDAY):");
-        String userInput = input.nextLine().trim().toUpperCase();
+        String userInput = input.nextLine().trim().toUpperCase(); // kullanıcıdan alınan girdideki boşluklar silindi ve harfleri büyütüldü
 
+
+// kullanıcıdan alınan girdinin enum sınıfımızdaki değerlerden başka bir değer olması durumunda hata olmasını engelliyoruz.
         try {
             Day selectedDay = Day.valueOf(userInput);
             System.out.println(selectedDay + " günü çalışma saatleri: " + selectedDay.getWorkingHours());
